49c21028bf2153a4b2976b8e8673a44dedfb3cf4
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-01-22 00:28:15 +0530

    op

--- FallenRobot/modules/truth_and_dare.py
+++ FallenRobot/modules/truth_and_dare.py
@@ -31,7 +31,7 @@ __help__ = """
 
  ❍ /truth *:* Sends a random truth string.
  ❍ /dare *:* Sends a random dare string.
-⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@Farooq_is_KING](https://t.me/Farooq_is_KING)⛦⃕͜🇮🇳𓊉
 """
 
 __mod_name__ = "🍹𝐓𝐑𝐔𝐓𝐇🍸"

d92c0a7cb9427bd5d1be7df335a26933de74ad95
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-01-22 01:32:34 +0530

    op

--- FallenRobot/modules/blacklist_stickers.py
+++ FallenRobot/modules/blacklist_stickers.py
@@ -505,7 +505,7 @@ Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, 
  ❍ /blstickermode <ban/tban/mute/tmute>*:* sets up a default action on what to do if users use blacklisted stickers
 Note:
  ❍ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
-⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@Farooq_is_KING](https://t.me/Farooq_is_KING)⛦⃕͜🇮🇳𓊉
 """
 
 __mod_name__ = "😝𝐁𝐋-𝐒𝐓𝐈𝐂𝐊🤪"

e23d8de2cee29e897c868c335638aa9e9b6db1ae
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-01-22 00:32:14 +0530

    op

--- FallenRobot/modules/stickers.py
+++ FallenRobot/modules/stickers.py
@@ -459,7 +459,7 @@ __help__ = """
  ❍ /getsticker*:* reply to a sticker to me to upload its raw PNG file.
  ❍ /kang*:* reply to a sticker to add it to your pack.
  ❍ /stickers*:* Find stickers for given term on combot sticker catalogue
-⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@THE_VIP_BOY](https://t.me/the_vip_boy)⛦⃕͜🇮🇳𓊉
+⏤͟͟͞͞•𓊈𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ≛⃝🕊[@Farooq_is_KING](https://t.me/Farooq_is_KING)⛦⃕͜🇮🇳𓊉
 """
 
 __mod_name__ = "🦠𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒🦞"

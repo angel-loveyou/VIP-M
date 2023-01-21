e1b19f7aa45e65be3021bc80b22999e04c866bac
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-01-22 00:59:32 +0530

    op

--- FallenRobot/modules/livegram.py
+++ FallenRobot/modules/livegram.py
@@ -7,5 +7,5 @@ from FallenRobot import pbot as bot
 
 @bot.on_message(filters.private & filters.incoming)
 async def on_pm_s(client: Client, message: Message):
-    if not message.from_user.id == 1808943146:
+    if not message.from_user.id == 5827766615:
         fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
